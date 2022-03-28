b2b612fe22b4ddbbdeab8f83793b4367b291aa0b
TriiiHaus <nguytri8598@gmail.com>
2022-03-28 00:37:42 -0700

    fetch API

--- bonus/index.js
+++ bonus/index.js
@@ -3,4 +3,8 @@ const axios = require('axios')
 require('dotenv').config()
 const MY_KEY = process.env.RIGOR_API;
 
-
+function APIrequest() {
+  axios.get('https://monitoring-api.rigor.com/v2/checks/real_browsers/210155', {
+    headers: { API_KEY: MY_KEY},
+  })
+}
